50085bc6a0812d0fb9807f0fd4a2764321e513c3
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-10 16:34:39 +0900

    Title: 달에서 무게 구하기, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241010/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int chu = 13;
+        double rate = 0.165;
+        System.out.printf("%d * %.6f = %.6f",chu,rate,chu*rate);
+    }
+}
