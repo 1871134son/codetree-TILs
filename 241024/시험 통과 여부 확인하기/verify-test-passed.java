f702ab02e8132affb2c84ea05521e8114fc66a95
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-24 16:33:52 +0900

    Title: 시험 통과 여부 확인하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/시험 통과 여부 확인하기/verify-test-passed.java
+++ 241024/시험 통과 여부 확인하기/verify-test-passed.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int point = sc.nextInt();
-        int more = 0;
-        if(point > 80){
+        if(point >= 80){
             System.out.println("pass");
         }
         else
-        System.out.println((100-more)+" more score");
+        System.out.println((80-point)+" more score");
     }
 }
