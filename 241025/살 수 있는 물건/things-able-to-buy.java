51826ead27f64cb4eef8013151101a4907ec0911
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-25 10:20:02 +0900

    Title: 살 수 있는 물건, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/살 수 있는 물건/things-able-to-buy.java
+++ 241025/살 수 있는 물건/things-able-to-buy.java
@@ -8,7 +8,7 @@ public class Main {
 
         if(n>=3000)
         System.out.println("book");
-        else if(3000>n>=1000)
+        else if(3000>n && n>=1000)
         System.out.println("mask");
         else
         System.out.println("no");
