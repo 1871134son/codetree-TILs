57e27b2430cec208f7471548f8b806c30185dae1
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-25 10:06:12 +0900

    Title: 시력 검사 2, Time: 139ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 241025/시력 검사 2/eye-test-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double av = sc.nextDouble();
+
+        if(av>1.0)
+        System.out.println("High");
+        else if(av>0.5)
+        System.out.println("Middle");
+        else
+        System.out.println("Low");
+    }
+}
