cf56c5ddfae6da9933c3da9b0152db1b40273031
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-30 10:20:05 +0900

    Title: 알파벳에 따른 평가, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241030/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 241030/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -6,19 +6,19 @@ public class Main {
 
         String n = sc.next();
 
-        if(n=='S'){
+        if(n.equals('S')){
             System.out.println("Superior");
         }
-        else if(n=='A'){
+        else if(n.equals('A')){
             System.out.println("B");
         }
-        else if(n=='B'){
+        else if(n.equals('B')){
             System.out.println("Excellent");
         }
-        else if(n=='C'){
+        else if(n.equals('C')){
             System.out.println("Good");
         }
-        else if(n=='D'){
+        else if(n.equals('D')){
             System.out.println("Usually");
         }
         else{
