6ba72024340d1ae3d3b56f97551614aee59c5460
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-30 09:52:17 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241030/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.nextString();
+
+        if(n=='S'){
+            System.out.println("Superior");
+        }
+        else if(n=='A'){
+            System.out.println("B");
+        }
+        else if(n=='B'){
+            System.out.println("Excellent");
+        }
+        else if(n=='C'){
+            System.out.println("Good");
+        }
+        else if(n=='D'){
+            System.out.println("Usually");
+        }
+        else{
+            System.out.println("Faulure");
+        }
+        
+    }
+}
