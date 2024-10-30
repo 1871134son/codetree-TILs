801d9229acb67a15b39ae947e1e1ed2f7baa9c1f
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-30 10:32:06 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241030/살 수 있는 물건 2/things-able-to-buy-2.java
+++ 241030/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
 
+        n>=0 && n < = 10000;
         if(n>=3000){
             System.out.println("book");
         }
         else if(n<3000 && n>=1000){
             System.out.println("mask");
         }
-        else
+        else if(n>=500 && n < 1000)
         System.out.println("pen");
     }
 }
