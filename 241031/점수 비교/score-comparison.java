837ebacb29030ba186225439041b4575662317a3
Jiho Son <92436202+1871134son@users.noreply.github.com>
2024-10-31 13:57:41 +0900

    Title: 점수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241031/점수 비교/score-comparison.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A.m = sc.nextInt();
+        int A.e = sc.nextInt();
+
+        int B.m = sc.nextInt();
+        int B.e = sc.nextInt();
+
+        if(A.m>B.m && A.e>B.e){
+            System.out.println(1);
+        }
+        else
+        System.out.println(0);
+    }
+}
